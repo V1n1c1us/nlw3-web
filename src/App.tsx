3197be4e2776a5dc6231ff3678e97be6ba8ef202
V1n1c1us <v.f.diehl@gmail.com>
2020-10-12 23:41:19 -0300

    Was created initial landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logo from './assets/img/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>opa</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logo} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Cerro Branco</strong>
+          <span>Rio Grande do Sul</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)"/>
+        </a>
+      </div>
     </div>
   );
 }
