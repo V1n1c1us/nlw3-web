ae86c6e690fe7ba46238f38bc03ff8ad22e02439
V1n1c1us <v.f.diehl@gmail.com>
2020-10-13 00:20:10 -0300

    Styling the OrphanagesMap page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import "../styles/pages/orphanages-map.css";
+
+import mapMarkerImg from '../assets/img/map-marker.svg';
 
 function OrphanagesMap() {
   return (
-    <h1>OrphanagesMap</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Cerro Branco</strong>
+          <span>Rio Grande do Sul</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={26} color="fff"/>
+      </Link>
+    </div>
   );
 }
 
