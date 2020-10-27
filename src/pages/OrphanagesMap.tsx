7cbd3388ea5681ac3b2377682b67a3542a318e85
V1n1c1us <v.f.diehl@gmail.com>
2020-10-27 15:49:26 -0300

    Move sidebar to component

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -53,14 +53,14 @@ function OrphanagesMap() {
             className="map-popup"
           >
             Lar teste
-            <Link to="">
+            <Link to="orphanages/1">
               <FiArrowRight size={20} color="#fff"/>
             </Link>
           </Popup>
         </Marker>
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={26} color="fff"/>
       </Link>
     </div>
